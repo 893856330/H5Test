b651ed192d13ad51e776ab845c5425f532fa8270
kailun.zhao <kailun.zhao@archermind.com>
2026-05-06 19:53:06 +0800

    新增：三消

---
+++ games/match-3/Main.js
@@ -0,0 +1,8 @@
+var config = {
+    width: 1500,
+    height: 700,
+    backgroundColor: 0x000000,
+    scene: [Load, Menu, LevelSelect, LevelMaker],
+}
+
+var game = new Phaser.Game(config);
---
+++ games/match-3/SpecialTiles/Block.js
@@ -0,0 +1,24 @@
+class Block extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture) {
+        super(scene, x, y, texture);
+        this.scene = scene;
+        this.scene.add.existing(this);
+        this.hardness = 1;
+        this.disableInteractive();
+    }
+
+    // Method to destroy the block
+    destroyBlock() {
+        this.destroy();
+    }
+
+    changeTexture(newTextureKey) {
+        this.setTexture(newTextureKey);
+    }
+
+    hardnessDecrease() {
+        this.hardness--;
+    }
+}
+
+//export default Block;
